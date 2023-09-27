7414a3099a69c643877590f966e878094725ddce
Ethan Glover <hello@ethang.email>
2023-09-27 09:51:29 -0500

    add astro linting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 
@@ -9,9 +9,9 @@ export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
   adapter: node({
-    mode: "standalone"
+    mode: "standalone",
   }),
   image: {
     domains: ["cdn.sanity.io"],
-  }
+  },
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,14 @@
-const {nextui} = require("@nextui-org/react");
+const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/@nextui-org/**/dist/**/*.{js,ts,jsx,tsx}'
-	],
-	theme: {
-		extend: {},
-	},
-	safelist: ['grid-cols-2'],
-	plugins: [
-		require('@tailwindcss/typography'),
-		nextui()
-	],
-}
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/@nextui-org/**/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  safelist: ["grid-cols-2"],
+  plugins: [require("@tailwindcss/typography"), nextui()],
+};

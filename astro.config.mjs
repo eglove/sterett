5323396351c0040ec342a35fd079c6689a6d162d
Ethan Glover <hello@ethang.email>
2023-09-27 12:25:25 -0500

    switch to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
-
 import react from "@astrojs/react";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
   image: {
-    domains: ["cdn.sanity.io"],
-  },
+    domains: ["cdn.sanity.io"]
+  }
 });
